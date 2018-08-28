cf037a3a404de0ca0a279261f65ef51fc6b40feb
Poudel <priyanka.poudel@capgemini.com>
2018-08-28 08:51:01 -0400

    first commit

---
+++ Student.java
@@ -0,0 +1,13 @@
+package com.capgemini.day5;
+
+public class Student {
+	private long rollNo;
+	private String name;
+	private int age;
+	private String course;
+	public Student() {
+		super();
+	}
+	public Student(long rollNo, String name, int age, String course) {
+	   
+}
